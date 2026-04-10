aed755e1d7c53443b4fb282379d0bbc2f7ba9b6f
smartinezhe <samartinez2191@gmail.com>
2026-04-10 15:09:12 -0500

    Update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://smartinezhe1.pages.dev',
+	site: 'https://smartinezhe.netlify.app/',
 	vite: {
 		plugins: [tailwindcss()],
 	},
